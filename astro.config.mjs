5aa7f0c8f872f485df806c6864d9c9ee4a075d92
Rama Javier Ramirez <jramirez2432@gmail.com>
2026-03-12 00:08:09 -0300

    last update from the firsts files of the prohect

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,12 @@
 
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
-
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
 	vite: { plugins: [tailwindcss()] },
 
-	integrations: [icon()],
+	integrations: [icon({ iconDir: "src/assets/icons" })],
 });
